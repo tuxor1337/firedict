92ea0a11eb6c6cac8bcf09c61b29a2d2c6622778
Thomas Vogt <tuxor1337@web.de>
2013-12-31 03:21:08 +0100

    Switch back to primitive arrays and binary insert and binary search respectively.

--- js/stardict.js
+++ js/stardict.js
@@ -1,35 +1,35 @@
 (function (GLOBAL) { 
     
-    function getUIntAt(arr, offs) {
+    function getUintAt(arr, offs) {
         out = 0;
         for (var j = offs; j < offs+4; j++) {
                 out <<= 8;
-                out |= arr.charCodeAt(j) & 0xff;
+                out |= arr[j] & 0xff;
         }
         return out;
     }
     
     function readUTF8String(bytes) {
         var ix = 0;
-        if( bytes.slice(0,3) == "\xEF\xBB\xBF") ix = 3;
+        if(bytes[0] == 0xEF && bytes[1] == 0xBB && bytes[2] == 0xBF) ix = 3;
         var string = "";
         for( ; ix < bytes.length; ix++ ) {
-            var byte1 = bytes[ix].charCodeAt(0);
+            var byte1 = bytes[ix];
             if( byte1 < 0x80 ) {
                 string += String.fromCharCode(byte1);
             } else if( byte1 >= 0xC2 && byte1 < 0xE0 ) {
-                var byte2 = bytes[++ix].charCodeAt(0);
+                var byte2 = bytes[++ix];
                 string += String.fromCharCode(((byte1&0x1F)<<6)
                     + (byte2&0x3F));
             } else if( byte1 >= 0xE0 && byte1 < 0xF0 ) {
-                var byte2 = bytes[++ix].charCodeAt(0);
-                var byte3 = bytes[++ix].charCodeAt(0);
-                string += String.fromCharCode(((byte1&0xFF)<<12) 
+                var byte2 = bytes[++ix];
+                var byte3 = bytes[++ix];
+                string += String.fromCharCode(((byte1&0xFF)<<12)
                     + ((byte2&0x3F)<<6) + (byte3&0x3F));
             } else if( byte1 >= 0xF0 && byte1 < 0xF5) {
-                var byte2 = bytes[++ix].charCodeAt(0);
-                var byte3 = bytes[++ix].charCodeAt(0);
-                var byte4 = bytes[++ix].charCodeAt(0);
+                var byte2 = bytes[++ix];
+                var byte3 = bytes[++ix];
+                var byte4 = bytes[++ix];
                 var codepoint = ((byte1&0x07)<<18) + ((byte2&0x3F)<<12)
                     + ((byte3&0x3F)<<6) + (byte4&0x3F);
                 codepoint -= 0x10000;
@@ -43,20 +43,127 @@
     }
     
     var StarDict = (function () {
-        var cls = function(db) {
-            var files = { }, index = [], synonyms, db = db;
-            var keywords = {
-                "version": "",
-                "bookname": "",
-                "wordcount": "",
-                "synwordcount": "",
-                "idxfilesize": "",
-                "sametypesequence": "",
-            };
-            var is_dz = false;
+        var cls = function(db, cmp_fct) {
+            var files = { },
+                index = [],
+                db = db,
+                keywords = {
+                    "version": "",
+                    "bookname": "",
+                    "wordcount": "",
+                    "synwordcount": "",
+                    "idxfilesize": "",
+                    "sametypesequence": "",
+                    "is_dz": false,
+                    "dbwordcount": 0
+                },
+                cmp_func = cmp_fct || function (a,b) {
+                    a = a.toLowerCase(), b = b.toLowerCase();
+                    if(a > b) return 1;
+                    if(a < b) return -1;
+                    return 0;
+                };
             var that = this;
-            var dict;
-            var progress = 0;
+            var progress = 0; // for debugging purposes, see process_syn
+                              // and process_idx respectively
+            
+            function getSynonymAt(offset) {
+                var reader = new FileReaderSync(),
+                    buffer = reader.readAsArrayBuffer(files["syn"]),
+                    view = new Uint8Array(buffer, offset),
+                    term = "", wid = -1;
+                for(var i = 0; i < view.length; i++) {
+                    if(view[i] == 0) {
+                        wid = getUintAt(view, i+1);
+                        term = readUTF8String(view.subarray(0,i));
+                        break;
+                    }
+                }
+                return [term, wid];
+            }
+            
+            function getIndexById(wid) {
+                var offset = index[wid],
+                    reader = new FileReaderSync(),
+                    buffer = reader.readAsArrayBuffer(files["idx"]),
+                    view = new Uint8Array(buffer, offset),
+                    term = "", offset = -1, size = 0;
+                for(var i = 0; i < view.length; i++) {
+                    if(view[i] == 0) {
+                        term = readUTF8String(view.subarray(0,i));
+                        offset = getUintAt(view, i+1);
+                        size = getUintAt(view, i+5);
+                        break;
+                    }
+                }
+                return [term, offset, size];
+            }
+            
+            function getTermFromObj(obj) {
+                if(obj.type == 0) return getIndexById(obj.offset)[0];
+                else {
+                    return getSynonymAt(obj.offset)[0];
+                }
+            }
+            
+            function decodeObj(obj) {
+                var idx, synonym, result = [];
+                if(obj.type == 0) {
+                    idx = getIndexById(obj.offset);
+                    synonym = idx[0];
+                } else {
+                    synonym = getSynonymAt(obj.offset);
+                    idx = getIndexById(synonym[1]);
+                    synonym = synonym[0];
+                }
+                result = [synonym].concat(idx);
+                result.push(db.did);
+                return result;
+            }
+            
+            function binaryInsert(arr, newElement) {
+                var minIndex = 0;
+                var maxIndex = arr.length - 1;
+                var currentIndex;
+                var currentCmp;
+                
+                while (minIndex <= maxIndex) {
+                    currentIndex = (minIndex + maxIndex) / 2 | 0;
+                    currentCmp = cmp_func(arr[currentIndex][0], newElement[0]);
+                    
+                    if (currentCmp == -1) {
+                        minIndex = currentIndex + 1;
+                    } else if (currentCmp == 1) {
+                        maxIndex = currentIndex - 1;
+                    } else {
+                        minIndex = currentIndex;
+                        break;
+                    }
+                }
+                arr.splice(minIndex, 0, newElement);
+                return minIndex;
+            }
+            
+            function data_to_db(tmp_synonyms) {
+                var tmp_arr = [];
+                for(var i = 0; i < tmp_synonyms.length; i++) {
+                    var syn = tmp_synonyms[i];
+                    tmp_synonyms[i] = { id: i, type: syn[1], offset: syn[2] };
+                }
+                db.store_synonyms(tmp_synonyms, function () {
+                    keywords.dbwordcount = tmp_synonyms.length;
+                    delete tmp_synonyms;
+                    db.backup_idx(index, process_res);
+                });
+            }
+            
+            function restore_from_db() {
+                console.log("restoring...");
+                db.restore_idx(function (idx) {
+                    index = idx;
+                    process_res();
+                });
+            }
             
             function process_res() {
                 if(files["res"].length > 0) {
@@ -71,64 +178,50 @@
                 }
             }
             
-            function data_to_db(tree) {
-                console.log("writing to db...");
-                tree.toDB(db, progress, function (idb_tree) {
-                    synonyms = idb_tree;
-                    db.backup_idx(index, process_res);
-                });
-            }
-            
-            function process_syn(tree) {
+            function process_syn(tmp_synonyms) {
+                progress = 0;
                 if(files["syn"] != null) {
-                    var reader = new FileReaderSync();
-                    var blob = reader.readAsBinaryString(files["syn"]);
-                    function rec_syn(i, j) {
-                        if(i >= blob.length || progress > 0) data_to_db(tree);
-                        else if(blob[i] == "\0") {
-                            var synonym = readUTF8String(blob.slice(j,i));
-                            var wid = getUIntAt(blob,i+1);
-                            tree.addWidToPath(synonym, wid);
-                            ++progress;
-                            rec_syn(i+5, i+5);
-                        } else rec_syn(i+1, j);
+                    var reader = new FileReaderSync(),
+                        buffer = reader.readAsArrayBuffer(files["syn"]),
+                        view = new Uint8Array(buffer);
+                    for(var i = 0, j = 0; i < view.length; i++) {
+                        if(view[i] == 0) {
+                            //if(progress > 1000) break;
+                            //if(progress < 50000){i+=5;j=i;progress++;continue;}
+                            progress += 1;
+                            if(progress % 2743 == 0)
+                                reply("progress", progress, keywords.synwordcount);
+                            var term = readUTF8String(view.subarray(j,i));
+                            binaryInsert(tmp_synonyms, [term, 1, j]);
+                            i += 5; j = i;
+                        }
                     }
-                    rec_syn(0,0,0);
-                } else {
-                    data_to_db(tree)
-                }
+                    data_to_db(tmp_synonyms);
+                } else data_to_db(tmp_synonyms);
             }
             
             function process_idx() {
-                var reader = new FileReaderSync();
-                var blob = reader.readAsBinaryString(files["idx"]);
-                var tree = new DicTree();
-                function rec_idx(i, j) {
-                    if(i >= blob.length || progress > 10) process_syn(tree);
-                    else if(blob[i] == "\0") {
-                        var word = readUTF8String(blob.slice(j,i));
-                        var offset = getUIntAt(blob,i+1);
-                        var size = getUIntAt(blob,i+5);
-                        index.push([word,offset,size]);
-                        tree.addWidToPath(word, index.length-1);
-                        ++progress;
-                        rec_idx(i+9, i+9);
-                    } else rec_idx(i+1, j);
+                var reader = new FileReaderSync(),
+                    buffer = reader.readAsArrayBuffer(files["idx"]),
+                    view = new Uint8Array(buffer),
+                    tmp_synonyms = [];
+                for(var i = 1, j = 0; i < view.length; i++) {
+                    if(view[i] == 0) {
+                        //if(progress > 1000) break;
+                        //if(progress < 50000){i+=9;j=i;progress++;continue;}
+                        progress += 1;
+                        if(progress % 2867 == 0)
+                            reply("progress", progress, keywords.wordcount);
+                        var term = readUTF8String(view.subarray(j,i));
+                        binaryInsert(tmp_synonyms, [term, 0, index.length]);
+                        index.push(j);
+                        i += 9; j = i;
+                    }
                 }
-                rec_idx(0,0,0);
+                process_syn(tmp_synonyms);
             }
             
-            function restore_from_db() {
-                db.restore_idx(function (idx) {
-                    index = idx;
-                    db.get_root(function (root) {
-                        synonyms = new DicTree_IDB(db)
-                        synonyms.fromData(root, process_res);
-                    });
-                });
-            }
-            
-            function process_ifo(from_idb) {
+            function process_ifo() {
                 reader = new FileReaderSync();
                 lines = reader.readAsText(files["ifo"]).split("\n");
                 if(lines.shift() != "StarDict's dict ifo file") {
@@ -139,23 +232,23 @@
                     w = l.split("=");
                     keywords[w[0]] = w[1];
                 });
-                if(from_idb) restore_from_db();
+                if(keywords.dbwordcount > -1) restore_from_db();
                 else process_idx();
             }
             
             function process_dictdata(data, callbk) {
+                var is_sts = false, data_arr = [];
                 if("" != keywords["sametypesequence"]) {
                     type_str = keywords["sametypesequence"];
                     is_sts = true;
                 }
-                data_arr = [];
                 while(true) {
                     if(is_sts) {
                         t = type_str[0];
                         type_str = type_str.substr(1);
                     } else {
-                        t = data[0];
-                        data = data.substr(1);
+                        t = String.fromCharCode(data[0]);
+                        data = data.slice(1);
                     }
                     if(is_sts && "" == type_str) d = data;
                     else if(t == t.toUpperCase()) {
@@ -163,7 +256,7 @@
                         d = data.slice(4,end+4);
                         data = data.slice(end+4);
                     } else {
-                        end = data.indexOf("\0");
+                        end = data.indexOf(0);
                         d = data.slice(0,end);
                         data = data.slice(end+1);
                     }
@@ -179,8 +272,8 @@
             this.onsuccess = function () { };
             this.loaded = false;
             
-            this.load = function (main_files, res_files, from_idb) {
-                var from_idb = from_idb || false;
+            this.load = function (main_files, res_files, dbwordcount) {
+                keywords.dbwordcount = dbwordcount || -1;
                 if(typeof res_files === "undefined") res_files = [];
                 files["res"] = res_files;
                 ["idx","syn","dict","dict.dz","ifo"].forEach(function(d) {
@@ -200,51 +293,84 @@
                     this.onerror("Missing *.idx file!");
                     return;
                 }
-                if(files["dict"] != null) is_dz = false;
-                else if(files["dict.dz"] != null) is_dz = true;
+                
+                if(files["dict"] != null) keywords.is_dz = false;
+                else if(files["dict.dz"] != null) keywords.is_dz = true;
                 else {
                     this.onerror("Missing *.dict(.dz) file!");
                     return;
                 }
-                process_ifo(from_idb);
+                process_ifo();
             };
             
-            this.lookup_id = function (wid, callbk) {
-                if(wid > index.length) callbk(null,null);
-                var idx = index[wid];
-                if(is_dz) {
+            this.get_entry = function (decodedObj, callbk) {
+                var offset = decodedObj[2],
+                    size = decodedObj[3];
+                if(keywords.is_dz) {
                     var reader = new DictZipFile(JSInflate.inflate);
                     reader.onsuccess = function () {
-                        process_dictdata(
-                            reader.read(idx[1], idx[2]),
-                            function(output) { callbk(output, idx); }
-                        );
+                        var binstr = reader.read(offset, size),
+                            intarr = [];
+                        for(var i = 0; i < binstr.length; i++) {
+                            intarr.push(binstr.charCodeAt(i));
+                        }
+                        process_dictdata(intarr, callbk);
                     };
                     reader.load(files["dict.dz"]);
                 } else {
-                    f = files["dict"].slice(
-                        idx[1], idx[1] + idx[2]
-                    );
-                    var reader = new FileReaderSync();
-                    process_dictdata(
-                        reader.readAsBinaryString(f),
-                        function(output) { callbk(output, idx); }
-                    );
+                    f = files["dict"].slice(offset, offset + size);
+                    var reader = new FileReaderSync(),
+                        buffer = reader.readAsArrayBuffer(f),
+                        view = new Uint8Array(buffer);
+                    process_dictdata(view, callbk);
                 }
             };
             
             this.lookup_term = function (word, callbk, fuzzy) {
-                if("undefined" === typeof fuzzy) fuzzy = false;
-                synonyms.jumpToPath(word, function (node) {
-                    if(fuzzy) {
-                        if(node == null) callbk([]);
-                        else node.deepSearch(20, callbk);
+                if(typeof fuzzy === "undefined") fuzzy = false;
+                function cmp(obj) {
+                    var term = getTermFromObj(obj);
+                    if(fuzzy) term = term.substr(0, word.length);
+                    return cmp_func(term, word);
+                }
+                var minIndex = 0,
+                    maxIndex = keywords.dbwordcount-1,
+                    currentIndex,
+                    currentObj = null,
+                    lastMatch = null,
+                    currentCmp;
+                
+                function binary_rec() {
+                    if(minIndex <= maxIndex) {
+                        currentIndex = (minIndex + maxIndex) / 2 | 0;
+                        db.get(currentIndex, function (obj) {
+                            currentObj = obj;
+                            currentCmp = cmp(currentObj);
+                            if (currentCmp == -1) {
+                                minIndex = currentIndex + 1;
+                            } else {
+                                if(currentCmp != 1) lastMatch = obj;
+                                maxIndex = currentIndex - 1;
+                            }
+                            binary_rec();
+                        });
                     } else {
-                        if(node == null || node.word_ids.length == 0)
-                            callbk(null);
-                        else callbk(node.word_ids[0]);
+                        if(lastMatch == null) callbk([]);
+                        else {
+                            db.get_range(lastMatch.id, 20, function(range) {
+                                var result = [];
+                                for(var r = 0; r < range.length; r++) {
+                                    currentObj = range[r];
+                                    currentCmp = cmp(currentObj); 
+                                    if(currentCmp != 0) break;
+                                    else result.push(decodeObj(currentObj));
+                                }
+                                callbk(result);
+                            });
+                        }
                     }
-                });
+                }
+                binary_rec();
             };
             
             this.add_resources = function (res_filelist) {
